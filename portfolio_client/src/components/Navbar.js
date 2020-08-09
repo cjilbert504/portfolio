97ced2e80a3d52750aee8212c6c2fc3a2eb40339
Collin Jilbert <cjilbert504@gmail.com>
2020-08-09 14:29:21 -0500

    Import Link from react-router-dom for use in navbar

--- portfolio_client/src/components/Navbar.js
+++ portfolio_client/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
     return (
         <div className="ui container">
             <div className="ui fixed pointing large menu">
-                <a href="#" className="header item">Collin Jilbert</a>
-                <a href="#" className="item">Home</a>
-                <a href="#" className="item">Blog</a>
-                <a href="#" className="item">Projects</a>
-                <a href="#" className="item">Resume</a>
-                <a href="#" className="item">Contact</a>
+                <Link to="/" className="header item">Collin Jilbert</Link>
+                <Link to="/" className="item">Home</Link>
+                <Link to="/blogs" className="item">Blog</Link>
+                <Link to="/projects" className="item">Projects</Link>
+                <Link to="/resumes" className="item">Resume</Link>
+                <Link to="/contact" className="item">Contact</Link>
             </div>
         </div>
     );
