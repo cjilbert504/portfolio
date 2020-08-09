368ebcfe9481adbf3b810626d83dce64b43b0d49
Collin Jilbert <cjilbert504@gmail.com>
2020-08-09 12:27:10 -0500

    Stub out content segment component

---
+++ portfolio_client/src/components/ContentSegment.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ContentSegment = () => {
+    return (
+        <div className="ui container" style={{ marginTop: "100px" }}>
+            <div className="ui very padded blue inverted raised segment">
+                <p>
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, 
+                    feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
+                    Mauris placerat eleifend leo.
+                </p>
+                <p>
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, 
+                    feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
+                    Mauris placerat eleifend leo.
+                </p>
+                <p>
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, 
+                    feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
+                    Mauris placerat eleifend leo.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default ContentSegment;
