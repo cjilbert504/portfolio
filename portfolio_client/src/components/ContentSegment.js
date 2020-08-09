b2e063ba720b89aca9076c94353996a56daeda77
Collin Jilbert <cjilbert504@gmail.com>
2020-08-09 12:43:42 -0500

    Add box-shadow to component

--- portfolio_client/src/components/ContentSegment.js
+++ portfolio_client/src/components/ContentSegment.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContentSegment = () => {
     return (
         <div className="ui container" style={{ marginTop: "100px" }}>
-            <div className="ui very padded blue inverted raised segment">
+            <div className="ui very padded blue inverted raised segment" style={{ boxShadow: "5px 5px 10px #888888" }}>
                 <p>
                     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, 
                     feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
