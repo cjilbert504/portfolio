1c9d7a44ffac94454f3158b671266392f10a6413
Collin Jilbert <cjilbert504@gmail.com>
2020-08-09 12:25:41 -0500

    Import content segment component

--- portfolio_client/src/components/App.js
+++ portfolio_client/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
+import ContentSegment from './ContentSegment';
 
 const App = () => {
     return (
         <div>
             <Navbar />
+            <ContentSegment />
+            <ContentSegment />
         </div>
     );
 };
