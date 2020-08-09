f7496fe1f16dc68e00b44247ae97a5b976bfa6d3
Collin Jilbert <cjilbert504@gmail.com>
2020-08-09 10:45:23 -0500

    Create navbar component

---
+++ portfolio_client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="ui container">
+            <div className="ui stackable menu">
+                <a href="#" className="item">Collin Jilbert</a>
+                <a href="#" className="item">Home</a>
+                <a href="#" className="item">Blog</a>
+                <a href="#" className="item">Projects</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
