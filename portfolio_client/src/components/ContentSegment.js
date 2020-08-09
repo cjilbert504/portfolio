723f5d5d11b5a6d58317344ac1f5156f18ca5f77
Collin Jilbert <cjilbert504@gmail.com>
2020-08-09 14:28:28 -0500

    Refactored to use props for reusability

--- portfolio_client/src/components/ContentSegment.js
+++ portfolio_client/src/components/ContentSegment.js
@@ -1,23 +1,12 @@
 import React from 'react';
 
-const ContentSegment = () => {
+const ContentSegment = (props) => {
     return (
         <div className="ui container" style={{ marginTop: "100px" }}>
             <div className="ui very padded blue inverted raised segment" style={{ boxShadow: "5px 5px 10px #888888" }}>
+                {props.children}
                 <p>
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, 
-                    feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
-                    Mauris placerat eleifend leo.
-                </p>
-                <p>
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, 
-                    feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
-                    Mauris placerat eleifend leo.
-                </p>
-                <p>
-                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, 
-                    feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
-                    Mauris placerat eleifend leo.
+                    {props.text}
                 </p>
             </div>
         </div>
