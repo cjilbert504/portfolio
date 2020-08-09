43821313f9aa13ff0eaab5e78bc4d1eca64edae9
Collin Jilbert <cjilbert504@gmail.com>
2020-08-09 12:26:37 -0500

    Add classes and fill out menu bar links

--- portfolio_client/src/components/Navbar.js
+++ portfolio_client/src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const Navbar = () => {
+const Navbar = (props) => {
     return (
         <div className="ui container">
-            <div className="ui stackable menu">
-                <a href="#" className="item">Collin Jilbert</a>
+            <div className="ui fixed pointing large menu">
+                <a href="#" className="header item">Collin Jilbert</a>
                 <a href="#" className="item">Home</a>
                 <a href="#" className="item">Blog</a>
                 <a href="#" className="item">Projects</a>
+                <a href="#" className="item">Resume</a>
+                <a href="#" className="item">Contact</a>
             </div>
         </div>
     );
