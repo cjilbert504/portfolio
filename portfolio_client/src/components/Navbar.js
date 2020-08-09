6014eab85f79ce9f423fe8210ca8e21a0149de6e
Collin Jilbert <cjilbert504@gmail.com>
2020-08-09 16:22:26 -0500

    Add social media links to navbar

--- portfolio_client/src/components/Navbar.js
+++ portfolio_client/src/components/Navbar.js
@@ -9,8 +9,19 @@ const Navbar = (props) => {
                 <Link to="/" className="item">Home</Link>
                 <Link to="/blogs" className="item">Blog</Link>
                 <Link to="/projects" className="item">Projects</Link>
-                <Link to="/resumes" className="item">Resume</Link>
+                <Link to="/resume" className="item">Resume</Link>
                 <Link to="/contact" className="item">Contact</Link>
+                <div className="right menu">
+                    <a href="https://github.com/cjilbert504" className="right floated item">
+                        <i className="github icon"></i>
+                    </a>
+                    <a href="https://twitter.com/collin_jilbert" className="right floated item">
+                        <i className="twitter icon"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/collin-jilbert/" className="right floated item">
+                        <i className="linkedin icon"></i>
+                    </a>
+                </div>
             </div>
         </div>
     );
