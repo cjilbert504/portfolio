47bc97de16e128ea3c98c3760ac593094daa38cf
Collin Jilbert <cjilbert504@gmail.com>
2020-08-09 21:58:42 -0500

    Build out beginning blog component

--- portfolio_client/src/components/containers/Blog.js
+++ portfolio_client/src/components/containers/Blog.js
@@ -3,18 +3,38 @@ import ContentSegment from '../ContentSegment';
 import railsAPI from '../../apis/railsAPI';
 
 class Blog extends React.Component {
+    state = {
+        blogPosts: []
+    };
+
     componentDidMount() {
         this.onPageRender();
     };
 
     onPageRender = async () => {
         const response = await railsAPI.get("/blog_posts")
-        console.log(response)
-    }
+        this.setState({ blogPosts: response.data })
+        console.log(this.state.blogPosts);
+    };
+
+    renderBlogPosts = () => {
+        return this.state.blogPosts.map(blog => (
+            <div key={blog.id}>
+                <div>
+                    <h1>{blog.title}</h1>
+                </div>
+                <div>
+                    {blog.content}
+                </div>
+            </div>
+        ))
+    };
 
     render() {
         return (
-            <ContentSegment />
+            <ContentSegment>
+                {this.renderBlogPosts()}
+            </ContentSegment>
         )
     }
 };
