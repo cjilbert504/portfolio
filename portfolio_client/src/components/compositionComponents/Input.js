b551e4ab52e85a29e28176b74c666bc102d235fe
Collin Jilbert <cjilbert504@gmail.com>
2020-08-15 22:27:00 -0500

    Add option to render different input types

--- portfolio_client/src/components/compositionComponents/Input.js
+++ portfolio_client/src/components/compositionComponents/Input.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 const Input = (props) => {
-    return (
-        <div className={props.className}>
-            <label>{props.labelText}</label>
-            <input placeholder={props.placeholder} type="text" />
-        </div>
-    );
+    if (props.type === "textarea") {
+        return (
+            <div className={props.className}>
+                <label>{props.labelText}</label>
+                <textarea placeholder={props.placeholder} type={props.type} />
+            </div>
+        );
+    } else {
+        return (
+            <div className={props.className}>
+                <label>{props.labelText}</label>
+                <input placeholder={props.placeholder} type={props.type} />
+            </div>
+        );
+    }
 };
 
 export default Input;
