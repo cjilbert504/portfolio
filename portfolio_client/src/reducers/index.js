69aedd8d6e60cd2e442072a7b9ec2e280bf90e21
Collin Jilbert <cjilbert504@gmail.com>
2020-08-15 22:28:40 -0500

    Add form reducer

--- portfolio_client/src/reducers/index.js
+++ portfolio_client/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import blogsReducer from './blogs_reducer';
 
+
 const rootReducer = combineReducers({
-    blogs: blogsReducer
+    blogs: blogsReducer,
+    form: formReducer
 });
 
 export default rootReducer;
